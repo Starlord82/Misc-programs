e3894e30bc03db74ad9ea630055d891e40acfad1
Starlord82 <zvikadov@gmail.com>
2021-08-03 15:51:52 +0300

    update

--- Pregnancy weeks.py
+++ Pregnancy weeks.py
@@ -1,17 +1,13 @@
-import datetime
+import datetime as dt
 
-period = input('Whats the first day of the last period? (dd/mm/yyyy): ')
 
 def week_by_date():
-	day, month, year = map(int,period.split('/'))
 
 	f_date = input('Enter future date: (dd/mm/yyyy): ')
 
 	f_day, f_month, f_year = map(int, f_date.split("/"))
-
-
-	p_date = datetime.datetime(year, month, day)
-	f_date = datetime.datetime(f_year, f_month, f_day)
+	
+	f_date = dt.datetime(f_year, f_month, f_day)
 
 	weeks = int(((f_date - p_date).days)/7)
 
@@ -19,28 +15,35 @@ def week_by_date():
 
 def date_by_week():
 
-	weeks = input('Enter week: ')
-	days = weeks * 7
-	f_date = p_date + datetime.timedelta(days = days)
+	weeks = int(input('Enter week: '))
+	f_days = weeks * 7
 
-	print(f_date)
+	f_date = p_date + dt.timedelta(weeks=weeks)
+
+	f_day, f_month, f_year = (f_date.day, f_date.month, f_date.year)
+
+	print(f'Week {weeks} will be at:\n{f_day}\\{f_month}\\{f_year}')
 
 if __name__ == "__main__":
 
 	while True:
+		period = input('Whats the first day of the last period? (dd/mm/yyyy): ')
+		print("\n"*3)
+		day, month, year = map(int,period.split('/'))
+		p_date = dt.datetime(year, month, day)
 
-		choice = input('Enter "1" for week by future date, or "2" for date by week')
+		choice = input('Enter "1" for week by future date, or "2" for date by week: ')
 
 		if choice == "1":
 
 			week_by_date()
 			break
 
-	 	elif choice == "2":
+		elif choice == "2":
 
-	 		date_by_week()
-	 		break
-	 	else:
-	 		print('wrong input!')
-	 		continue
+			date_by_week()
+			break
+		else:
+			print('wrong input!')
+			continue
 
