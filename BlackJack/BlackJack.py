a733caab4941cf9039bdb9979bf72af4e126b54e
Starlord82 <zvikadov@gmail.com>
2021-08-04 21:28:09 +0300

    Update BlackJack.py

--- BlackJack/BlackJack.py
+++ BlackJack/BlackJack.py
@@ -6,7 +6,7 @@
 
 #Sum function with *args to sum the total, check if theres an ace
 import random
-from os
+import os
 
 ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 values = {'2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9,
