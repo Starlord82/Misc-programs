3b0a00ad77fc20f911c41697a60d73bd770f6c1b
Starlord82 <zvikadov@gmail.com>
2021-08-15 13:15:18 +0300

    tests

--- Convert to excel/CTE.py
+++ Convert to excel/CTE.py
@@ -1,11 +1,13 @@
-import PyPDF2
+# CTE - Convert pdf To Excel
+
+# import PyPDF2
 import tabula
 from tkinter.filedialog import askopenfilename
+import os
+import camelot
+import pandas as pd
 
-
-
-# df = tabula.read_pdf("Y:\dwg\משרד שיכון\מחירון\documents_tichnun_ironi_infrastructure_and_paving_price_list.pdf", pages='all')
 filename = askopenfilename()
-tabula.io.convert_into(filename, 'output.csv', pages = '74-76')
+tables = tabula.io.read_pdf(filename, stream = True)
 
-print('Finished')
+print(tables)
