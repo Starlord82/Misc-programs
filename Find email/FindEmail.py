cab34888ee3b5377ffe0af19a6784ecc27f06b4e
Starlord82 <zvikadov@gmail.com>
2021-08-22 10:01:34 +0300

    Update FindEmail.py

--- Find email/FindEmail.py
+++ Find email/FindEmail.py
@@ -2,15 +2,17 @@ import os
 import extract_msg
 import re
 import shutil
+from tkinter import filedialog
 
-os.chdir('C:\\Users\\data121\\Dropbox\\דרך הנדסה\\הנדסה\\27_00\\2714\\2714-1\\תכתובת')
-
-output_path = 'C:\\Users\\data121\\Desktop\\results'
+input_path = filedialog.askdirectory(title = "Choose folder to search in")
+output_path = filedialog.askdirectory(title = "Choose where to copy matches")
 
 counter = 0
+search_counter = 0
 
 def find_word(file,word):
    global counter
+   global search_counter
    msg = extract_msg.openMsg(file)
    text = msg.body
    print(f'opening msg {file}')
@@ -18,12 +20,14 @@ def find_word(file,word):
       counter += 1
       shutil.copy(file, output_path)
       print('msg copied')
+   search_counter += 1
 
 
-for folder, subfolder, file in os.walk(os.getcwd()):
+for folder, subfolder, file in os.walk(input_path):
    for f in file:
       file_path = folder + "\\" + f
       if re.search(r'\w+.msg', file_path):
          find_word(file_path, "מגרסה")
 
+print(f'{search_counter} files have been searched')
 print(f'{counter} files have been copied')
