6c4bfb2ffdc4c18861622b57948f2ba54e079eb2
Starlord82 <87604535+Starlord82@users.noreply.github.com>
2021-10-20 13:32:58 +0300

    fix duplication issue

--- Find email/FindEmail.py
+++ Find email/FindEmail.py
@@ -6,6 +6,7 @@ from tkinter import filedialog
 
 input_path = filedialog.askdirectory(title = "Choose folder to search in")
 output_path = filedialog.askdirectory(title = "Choose where to copy matches")
+word = input("Enter Keyword you want to search: ")
 
 counter = 0
 search_counter = 0
@@ -18,16 +19,19 @@ def find_word(file,word):
    print(f'opening msg {file}')
    if re.search(word, text):
       counter += 1
-      shutil.copy(file, output_path)
+      filename = file.split("\\")[-1].split(".")[0]+"-"+str(counter)+".msg"
+      shutil.copy(file, output_path+"\\"+filename)
       print('msg copied')
    search_counter += 1
+
 
 
 for folder, subfolder, file in os.walk(input_path):
    for f in file:
       file_path = folder + "\\" + f
       if re.search(r'\w+.msg', file_path):
-         find_word(file_path, "מגרסה")
+         find_word(file_path, word)
+
 
 print(f'{search_counter} files have been searched')
 print(f'{counter} files have been copied')
